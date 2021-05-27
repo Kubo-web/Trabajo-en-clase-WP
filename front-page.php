d3842b95e82f09d10f372af1c1af4408ae96a0a2
Juan Pablo Ortiz <juanpablo.50cn@hotmail.com>
2021-05-26 21:29:34 -0500

    Página Responsive

--- front-page.php
+++ front-page.php
@@ -1,4 +1,100 @@
 <?php get_header(); ?>
 
+
+<nav class="flex justify-between bg-green-300 items-center text-black-200 px-4 shadow-md sm:py-4 sm:justify-around">
+            <div class="sm:hidden text-2xl">&equiv;</div>
+            <ul class="hidden sm:flex justify-between w-64">
+                <li>
+                    <a href="http://">Inicio</a>
+                </li>
+                <li>
+                    <a href="http://">Categorías</a>
+                </li>
+                <li>
+                    <a href="http://">Acerca de mi</a>
+                </li>
+            </ul>
+            <ul class="flex justify-between w-20">
+                <li>
+                    <a href="http://">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://">
+                        <i class="fas fa-rss"></i>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+
+
+
+<div class="mt-10 mx-auto bg-white shadow-xl overflow-hidden md:max-w-6xl">
+    
+        <div class="bg-green-300">
+        <img
+        class="p-3 mx-auto rounded h-48 w-full object-cover md:h-full md:w-48"
+        src="<?php echo get_template_directory_uri() ?>/assets/img/2.jpg" 
+        alt="imagen de perfil">
+        </div>
+        
+        
+        <div class="p-5 sm:text-center ">
+      <p class="text-black-500">
+      Hola soy Juan Pablo Ortiz, estudie en el instituto tecnológico 
+      Quito, se desarrollar software en java, 
+      para las páginas web sé trabajar con php y ahora 
+       estoy aprendiendo wordpress.</p>
+       </div>
+      </div>
+      <div class="mt-10 mx-auto bg-white shadow-xl overflow-hidden md:max-w-6xl">
+      <div class="bg-green-300">
+        <img
+        class="p-3 mx-auto rounded h-48 w-full object-cover md:h-full md:w-48"
+        src="<?php echo get_template_directory_uri() ?>/assets/img/3.jpg" 
+        alt="imagen de perfil">
+        </div>
+        
+        
+        <div class="p-5 sm:text-center ">
+      <p class="text-black-500">
+      Java es un lenguaje de programación y una plataforma informática 
+      comercializada por primera vez en 1995 por Sun Microsystems
+      . ... Java es rápido, seguro y fiable. 
+      Desde portátiles hasta centros de datos, 
+      desde consolas para juegos hasta súper computadoras, 
+      desde teléfonos móviles hasta Internet, Java está en 
+      todas partes..</p>
+       </div>
+      </div>
+      
+      </div>
+      <div class="mt-10 mx-auto bg-white shadow-xl overflow-hidden md:max-w-6xl">
+      <div class="bg-green-300">
+        <img
+        class="p-3 mx-auto rounded h-48 w-full object-cover md:h-full md:w-48"
+        src="<?php echo get_template_directory_uri() ?>/assets/img/4.png" 
+        alt="imagen de perfil">
+        </div>
+        
+        
+        <div class="p-5 sm:text-center ">
+      <p class="text-black-500">
+      PHP es un lenguaje de programación de uso general que se adapta especialmente al desarrollo web.​ 
+      Fue creado inicialmente por el programador danés-canadiense 
+      Rasmus Lerdorf en 1994.​ En la actualidad, 
+      la implementación de referencia de PHP es producida por 
+      The PHP Group</p>
+       </div>
+      </div>
+      
+      </div>
+
 <?php get_footer(); ?>
 
